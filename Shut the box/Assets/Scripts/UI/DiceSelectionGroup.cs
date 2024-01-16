b24c962e4212e9f3223750aabff82a6577b1cfbd
AnastasiaValishina <azhidenova@gmail.com>
2024-01-16 17:54:54 +0100

    hud ui update

---
+++ Shut the box/Assets/Scripts/UI/DiceSelectionGroup.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DiceSelectionGroup : MonoBehaviour
+{
+    [SerializeField] GameObject firstDice;
+    [SerializeField] GameObject secondDice;
+    [SerializeField] Button leftArrow;
+    [SerializeField] Button rightArrow;
+}
